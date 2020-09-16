fb79ac5966a552c0119e3ecf552ce5e61b4454de
Therial Coline <coline.therial@supelec.fr>
2020-09-16 21:02:18 +0200

    Fix dates

--- src/app/post.ts
+++ src/app/post.ts
@@ -15,9 +15,9 @@ export class Post {
         post.id = postFromApi.id;
         post.title = postFromApi.title;
         post.body = postFromApi.body;
-        post.publishedAtString = postFromApi.published_at ? moment(postFromApi.published_at).format('MMMM Do YYYY, h:mm a') : null;
+        post.publishedAtString = postFromApi.published_at ? moment.utc(postFromApi.published_at).local().format('MMMM Do YYYY, h:mm a') : null;
         post.publishedAtDate = postFromApi.published_at ? new Date(postFromApi.published_at) : null;
-        post.createdAt = postFromApi.created_at ? moment(postFromApi.created_at).format('MMMM Do YYYY, h:mm a') : null;
+        post.createdAt = postFromApi.created_at ? moment.utc(postFromApi.published_at).local().format('MMMM Do YYYY, h:mm a') : null;
         post.user = postFromApi.user ? User.apiToModel(postFromApi.user) : null;
         return post;
     }
